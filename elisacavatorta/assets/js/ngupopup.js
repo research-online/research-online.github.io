d1ee16bdb732b4e92967329249ecc5a4a10bfc82
gtrovato <gtrovato@veracode.com>
2020-11-12 23:43:19 +0100

    added local copy for google fonts, moved communications to the bottom of the page and restored credits

--- elisacavatorta/assets/js/ngupopup.js
+++ elisacavatorta/assets/js/ngupopup.js
@@ -19,17 +19,18 @@
         // Automatically triggers the venobox_ngu popup whenever the hash #communications is in the URL
         if (window.location.hash) {
             if (window.location.hash.includes("communications")) {
-                $('.venobox_ngu').venobox({
-                    framewidth: '800px',                            // default: ''
-                    frameheight: '',                            // default: ''
-                    border: '0px',                             // default: '0'
-                    bgcolor: '#f49494',                          // King's College background
-                    spinColor: '#f49494',
-                    share: ['facebook', 'twitter'] // default: []
-                }).trigger('click');
+                setTimeout(function () {
+                    $('.venobox_ngu').venobox({
+                        framewidth: '800px',                            // default: ''
+                        frameheight: '',                            // default: ''
+                        border: '0px',                             // default: '0'
+                        bgcolor: '#f49494',                          // King's College background
+                        spinColor: '#f49494',
+                        share: ['facebook', 'twitter'] // default: []
+                    }).trigger('click')
+                }, 1000);
             }
         }
     });
-
 
 })(jQuery);
